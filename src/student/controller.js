b315dc2911012c2ebc5d5bd5c56ee63e3356c445
Muqaddas1987 <kamolovamuqaddas@gmail.com>
2023-06-03 15:54:36 +0500

    initial

---
+++ src/student/controller.js
@@ -0,0 +1,29 @@
+const pool = require("../../db");
+const queries = require("./queries");
+const getStudents = (req, res) => {
+  pool.query(queries.getStudents, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+const getStudentById = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+const addStudent = (req, res) => {
+  const { name, email, age, date_of_birth } = req.body;
+  // check email exists
+  pool.query(queries.checkEmailExists, [email], (error, results) => {
+    if (results.rows.length) {
+      res.send("Email already exists");
+    }
+  });
+};
+module.exports = {
+  getStudents,
+  getStudentById,
+  addStudent,
+};
---
+++ src/student/queries.js
@@ -0,0 +1,8 @@
+const getStudents = "SELECT * FROM students";
+const getStudentById = "SELECT * FROM students Where id = $1";
+const checkEmailExists = "SELECT s FROM students s WHERE s.email = $1";
+module.exports = {
+  getStudents,
+  getStudentById,
+  checkEmailExists,
+};
